c737f744bd401d540b80188030a20e6bba7d02ea
TechRaven18 <nsharma274206@gmail.com>
2026-07-31 22:52:29 +0530

    activity log file

---
+++ server/models/AuditLog.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const auditLogSchema = new mongoose.Schema({
+  actor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  action: {
+    type: String,
+    required: true
+  },
+  details: {
+    type: Object
+  },
+  ipAddress: String,
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('AuditLog', auditLogSchema);
